205d681630b426c1fe2b609b987dbe595d45017d
Dark <darkgrouptw@gmail.com>
2018-07-12 13:29:49 +0800

    1. 完成 HZRecorder 2. 搬移結果到裡面 3. 增加 Fullpage.js 的功能 4. 增加 Boostrap 的排版

--- js/main.js
+++ js/main.js
@@ -1,14 +1,18 @@
 var recorder = null;
 
+/*
+錄音專用
+*/
 function startRecording() 
 {
     HZRecorder.get(function (rec) {
         recorder = rec;
         recorder.start();
+        
+        // 測試用
         //recorder.testOutput($("#testPlay")[0]);
     });
 }
-
 function stopRecording()
 {
     recorder.stop();
@@ -17,3 +21,21 @@ function playRecording()
 {
     recorder.play($("#testPlay")[0]);
 }
+
+
+/*
+這邊是 Custom Button 的按鈕
+*/ 
+
+/*
+JQuery
+*/
+var FullPage = null;
+$(function(){
+    FullPage = new fullpage('#fullpage', {
+        sectionsColor: ['#4BBFC3'],
+        licenseKey: "OPEN-SOURCE-GPLV3-LICENSE",
+        controlArrows: false
+        //anchors: ["Introduction"]
+    });
+});
