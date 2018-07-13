65e46a1d1ffac61809412912853508bbc2f0a63d
Dark <darkgrouptw@gmail.com>
2018-07-13 21:25:59 +0800

    上傳排完版的版本

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
-var recorder = null;
+var recorder = null;            // 錄音變數
+var FullPageAPI = null;            // 頁面控制
 
 /*
 錄音專用
@@ -6,33 +7,82 @@ var recorder = null;
 function startRecording() 
 {
     HZRecorder.get(function (rec) {
-        recorder = rec;
-        recorder.start();
+        /*recorder = rec;
+        recorder.start();*/
         
-        // 測試用
-        //recorder.testOutput($("#testPlay")[0]);
+        // 文字
+        $("#before_recorder_div").hide();
+        $("#recording_div").show();
+        
     });
 }
 function stopRecording()
 {
-    recorder.stop();
-}
-function playRecording() 
-{
-    recorder.play($("#testPlay")[0]);
+    /*recorder.stop();
+    
+    // 測試播放用
+    recorder.play($("#testPlay")[0]);*/
+    
+    // 轉 Base64
+    /*var reader = new FileReader();
+    reader.readAsDataURL(recorder.getBlob()); 
+    reader.onloadend = function() {
+        base64data = reader.result;                
+        
+        var fd = new FormData();
+        fd.append("DeviceId", 0);
+        fd.append("Content", "Test");
+        fd.append("VoiceFile", "AAA");
+        var xhr = new XMLHttpRequest();
+        /*if (callback) {
+            xhr.upload.addEventListener("progress", function (e) {
+                callback('uploading', e);
+            }, false);
+            xhr.addEventListener("load", function (e) {
+                callback('ok', e);
+            }, false);
+            xhr.addEventListener("error", function (e) {
+                callback('error', e);
+            }, false);
+            xhr.addEventListener("abort", function (e) {
+                callback('cancel', e);
+            }, false);
+        }*/
+        /*xhr.addEventListener("load", function (e) {
+                console.log(e);
+            }, false);
+        xhr.open("POST", "https://message.waynehuang.ml/api/uploadMusic", true);
+        xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+        //xhr.setRequestHeader('Access-Control-Allow-Origin','*');
+        xhr.send(fd);   
+    }*/
+    
+    // 跳到另外一個頁面
+    RecordingFinish();
 }
 
 
 /*
 這邊是 Custom Button 的按鈕
 */ 
+function Introduction_Button_Press()
+{
+    if(FullPageAPI != null)
+        FullPageAPI.moveSlideRight();
+}
+function RecordingFinish()
+{
+    if(FullPageAPI != null)
+        FullPageAPI.moveSlideRight();
+}
+
+
 
 /*
 JQuery
 */
-var FullPage = null;
 $(function(){
-    FullPage = new fullpage('#fullpage', {
+    FullPageAPI = new fullpage('#fullpage', {
         sectionsColor: ['#4BBFC3'],
         licenseKey: "OPEN-SOURCE-GPLV3-LICENSE",
         controlArrows: false
